90a7e63740b265ff3fa7f5a11585f4207328eec7
Maciej Szwoch <maciejszwoch@yahoo.com>
2022-09-19 14:38:28 -0500

    Update gimme.sh
    Upload

--- gimme.sh
+++ gimme.sh
@@ -1,1 +1,12 @@
-s
+#!/bin/bash
+
+# Created to move searchsploit exploits to current working directory, but works on other files
+# Problems with files too similarly named Ex: /.../raft-medium-directories-lowercase.txt and /.../raft-medium-directories.txt
+# SHOULD only move one file or error. No error checking done, use at own discretion.
+
+# usage: gimme.sh rock you 70
+# usage: gimme.sh generator/13528.c
+# usage: gimme.sh /usr/share/dirb/wordlists/common.txt
+
+fullpath=$(locate "$@")
+test -f $fullpath && echo "Moving $fullpath to here" && cp $fullpath $(pwd)
